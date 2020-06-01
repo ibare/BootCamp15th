10023303200b85224cf536369e24aa27a188aaba
Kim mintae <ibare77@gmail.com>
2020-06-01 21:55:10 +0900

    부트캠프 15기 5회차(6월1일) 코드update

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const captureWebsite = require('capture-website');
+const axios = require('axios');
+const parse = require('node-html-parser').parse;
 const express = require('express')
 const app = express()
 const MongoClient = require('mongodb').MongoClient;
@@ -8,6 +10,31 @@ let db;
  
 app.use(express.json()) 
 app.use(express.static('public'));
+
+async function getDocumentInfo(url) {
+  const htmlString = await axios.get(url);
+  const htmlDOM = parse(htmlString.data, {
+    lowerCaseTagName: false,
+    script: false,
+    style: false,
+    pre: false,
+    comment: false  
+  });
+
+  const title = htmlDOM.querySelector('title').text
+  let description;
+
+  htmlDOM.querySelectorAll('meta').forEach(meta => {
+    if (meta.getAttribute('name') === 'description') {
+      description = meta.getAttribute('content');
+    }
+  });
+
+  return {
+    title, 
+    description,
+  };
+}
 
 MongoClient.connect(url, { useUnifiedTopology: true }, function(err, client) {
   console.log("Connected successfully to mongodb");
@@ -29,18 +56,20 @@ app.get('/api/bookmarks', async (req, res) => {
 app.post('/api/bookmarks', async (req, res) => {
   const bookmark = db.collection('bookmark');
   const targetUrl = req.body.url;
+  const siteInfo = await getDocumentInfo(targetUrl);
   const data = await captureWebsite.base64(targetUrl);
   const result = await bookmark.insertOne({
     url: targetUrl,
-    title: '',
-    description: '',
+    title: siteInfo.title,
+    description: siteInfo.description,
     thumbnail: data.toString('base64'),
-  })
+  });
+
   const { url, title, description } = result
 
   res.send({
     url,
     title,
     description,
-  })
+  });
 })
